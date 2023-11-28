794ed6a440188c2903c320a9ae84df789c8d64e6
sabinagulcikova <sabina.gulcikova@gmail.com>
2023-11-28 16:43:44 +0900

    configuration with successful training of a model

--- bayesian-net.py
+++ bayesian-net.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 from pgmpy.estimators import HillClimbSearch, BayesianEstimator, ParameterEstimator, BicScore, PC, K2Score, \
-    MmhcEstimator, TreeSearch
+    MmhcEstimator, TreeSearch, ExhaustiveSearch, MaximumLikelihoodEstimator, ExpectationMaximization
 from pgmpy.models import BayesianNetwork, BayesianModel
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
@@ -31,16 +31,16 @@ from sklearn.metrics import (
     f1_score,
     roc_auc_score,
     roc_curve,
-    auc,
+    auc, classification_report,
 )
 from tqdm import tqdm
 
 
 def parse_args() -> argparse.Namespace:
     args = argparse.ArgumentParser()
-    args.add_argument('--csv-path-train', type=str, default='data/train_data.csv',
+    args.add_argument('--csv-path-train', type=str, default='data/train_data_labels.csv',
                       help='Path to csv file of train dataset.')
-    args.add_argument('--csv-path-test', type=str, default='data/test_data.csv',
+    args.add_argument('--csv-path-test', type=str, default='data/train_data_labels.csv',
                       help='Path to csv file of test dataset.')
     args.add_argument('--net-structure-path', type=str, default='models/net_structure.pkl',
                       help='Path to file with trained structure of Bayesian net.')
@@ -63,8 +63,15 @@ class BayesianTrainer:
         self.net_structure_path = net_structure_path
         self.net_structure_and_params_path = net_structure_and_params_path
         self.batch_size = batch_size
-        # Load the model if it exists, else set it to None
         self.best_model = self.load_model_structure()
+        self.state_names = {
+            'tenure': [0, 1, 2, 3, 4],
+            'TotalCharges': [0, 1, 2, 3, 4],
+            'PaymentMethod': [0, 1, 2, 3],
+            'MonthlyCharges': [0, 1, 2, 3, 4],
+            'Contract': [0, 1, 2],
+            'Churn': [0, 1]
+        }
 
     def load_model_structure(self):
         if os.path.exists(self.net_structure_path):
@@ -79,13 +86,57 @@ class BayesianTrainer:
             return pickle.load(file)
 
     def structure_train(self) -> None:
+        print(f'----------------------------')
+        print("Starting structure learning...")
+        print(f'----------------------------')
+        #
+        # state_names = {
+        #     'MonthlyCharges': [0, 1, 2],
+        #     'TotalCharges': [0, 1, 2],
+        #     'tenure': [0, 1, 2],
+        #     'Churn': [0, 1],
+        #     'Contract': [0, 1, 2],
+        #     'PaymentMethod': [0, 1, 2, 3]
+        #     # 'OnlineSecurity_No internet service': [0, 1],
+        #     # 'TechSupport_No internet service': [0, 1],
+        #     # 'Contract_Month-to-month': [0, 1],
+        #     # 'Contract_Two year': [0, 1],
+        #     # 'PaymentMethod_Credit card (automatic)': [0, 1],
+        #     # 'PaymentMethod_Mailed check': [0, 1],
+        #     # 'PaymentMethod_Electronic check': [0, 1],
+        #     # 'PaymentMethod_Bank transfer (automatic)': [0, 1],
+        #     # 'TechSupport_No': [0, 1],
+        #     # 'Contract_One year': [0, 1],
+        #     # 'TechSupport_Yes': [0, 1]
+        # }
+
         # approach 1 HillClimbSearch
-        # hc = HillClimbSearch(self.train_df)
-        # structure = hc.estimate()
+        # hc = HillClimbSearch(self.train_df, state_names=self.state_names)
+        # learned_structure = hc.estimate()
 
         # approach 2 HillClimbSearch with k2
         # hc = HillClimbSearch(self.train_df)
-        # structure = hc.estimate(scoring_method=K2Score(self.train_df), tabu_length=150)
+        # learned_structure = hc.estimate(scoring_method=K2Score(self.train_df), tabu_length=150)
+
+        # tree_estimator = TreeSearch(self.train_df)
+        # learned_structure = tree_estimator.estimate()
+
+        # tree_estimator = TreeSearch(self.train_df)
+        # learned_structure = tree_estimator.estimate(estimator_type='tan', class_node='Churn')
+
+        # pc_estimator = PC(self.train_df)
+        # learned_structure = pc_estimator.estimate(ci_test='pearsonr', return_type='dag', show_progress=True)
+
+        # pc_estimator = PC(self.train_df)
+        # learned_structure = pc_estimator.estimate(ci_test='g_sq')
+
+        # mmhc_estimator = MmhcEstimator(self.train_df, state_names=self.state_names)
+        # learned_structure = mmhc_estimator.estimate(tabu_length=500)
+        # learned_structure = mmhc_estimator.estimate(significance_level=0.05)
+        # learned_structure = mmhc_estimator.estimate(scoring_method=BicScore(self.train_df))
+        # learned_structure = mmhc_estimator.estimate()
+
+
 
         # approach 4 MMHC this doesnt show any training progress
         # mmhc_estimator = MmhcEstimator(self.train_df)
@@ -95,46 +146,25 @@ class BayesianTrainer:
         # tree_estimator = TreeSearch(self.train_df)
         # learned_structure = tree_estimator.estimate(estimator_type='chow-liu')
 
-        # tree_estimator = TreeSearch(self.train_df)
-        # learned_structure = tree_estimator.estimate()
-
+        # Use K2Score as the scoring method (you can change it based on your preference)
+        scoring_method = K2Score(self.train_df)
         #
-        # tree_estimator = TreeSearch(self.train_df)
-        # learned_structure = tree_estimator.estimate(estimator_type='tan', class_node='Churn')
-
+        # # Initialize ExhaustiveSearch with data, scoring method, and state_names
+        es = ExhaustiveSearch(
+            self.train_df,
+            scoring_method=scoring_method,
+            state_names=self.state_names,
+            use_cache=True
+        )
         #
-        # pc_estimator = PC(self.train_df)
-        # learned_structure = pc_estimator.estimate(ci_test='chi_square')
+        # # Perform exhaustive search to find the best model
+        learned_structure = es.estimate()
+        print(f'----------------------------')
+        print("Structure learning completed.")
+        print(f'----------------------------')
 
-        # pc_estimator = PC(self.train_df)
-        # learned_structure = pc_estimator.estimate(ci_test='g_sq')
-
-        # NEXT
-        state_names = {
-            'TechSupport_No internet service': [0, 1],  # replace with actual state names for nodeA
-            'MonthlyCharges': [0, 1, 2, 3, 4],  # replace with actual state names for nodeB
-            'TotalCharges': [0, 1, 2, 3, 4],  # replace with actual state names for nodeB
-            'OnlineSecurity_No internet service': [0, 1],  # replace with actual state names for nodeB
-            'tenure': [0, 1, 2, 3, 4],  # replace with actual state names for nodeB
-            'Churn': [0, 1],  # replace with actual state names for nodeB
-            'Contract_Month-to-month': [0, 1],  # replace with actual state names for nodeB
-            'Contract_Two year': [0, 1],  # replace with actual state names for nodeB
-            'PaymentMethod_Credit card (automatic)': [0, 1],  # replace with actual state names for nodeB
-            'PaymentMethod_Mailed check': [0, 1],  # replace with actual state names for nodeB
-            'PaymentMethod_Electronic check': [0, 1],  # replace with actual state names for nodeB
-            'PaymentMethod_Bank transfer (automatic)': [0, 1],  # replace with actual state names for nodeB
-            'TechSupport_No': [0, 1],  # replace with actual state names for nodeB
-            'Contract_One year': [0, 1],  # replace with actual state names for nodeB
-            'TechSupport_Yes': [0, 1]  # replace with actual state names for nodeB
-            # Add other nodes as needed
-        }
-        mmhc_estimator = MmhcEstimator(self.train_df, state_names=state_names)
-        learned_structure = mmhc_estimator.estimate()
-        # NEXT
-
-
-        # Create a BayesianModel with the learned structure
-        self.best_model = BayesianModel(learned_structure.edges())
+        # Create a BayesianNetwork with the learned structure
+        self.best_model = BayesianNetwork(learned_structure.edges())
 
         # self.best_model = BayesianNetwork(structure.edges())
 
@@ -178,6 +208,10 @@ class BayesianTrainer:
             return pickle.load(file)
 
     def train_parameters(self):
+        # model.fit(df_train, estimator=MaximumLikelihoodEstimator)
+
+        # Doing exact inference using Variable Elimination
+        # infer = VariableElimination(model)
         model = self.best_model
 
         # Estimate CPDs
@@ -189,7 +223,7 @@ class BayesianTrainer:
                 batch_df = self.train_df.iloc[batch_start:batch_end]
 
                 # Estimate CPDs for the current batch
-                model.fit(batch_df, estimator=BayesianEstimator, prior_type='BDeu', equivalent_sample_size=10)
+                model.fit(batch_df, estimator=MaximumLikelihoodEstimator, state_names=self.state_names)
 
                 # model.fit(df_train, estimator=MaximumLikelihoodEstimator)
                 # infer = VariableElimination(model)
@@ -200,6 +234,7 @@ class BayesianTrainer:
         with open(self.net_structure_and_params_path, 'wb') as file:
             pickle.dump(model, file)
 
+        print(f' Model check: {model.check_model()}')
         return model
 
     def make_predictions(self, model):
@@ -209,47 +244,129 @@ class BayesianTrainer:
         for cpd in model.get_cpds():
             print(cpd)
 
+        trained_states = {}
+
+        for node in model.nodes():
+            states = model.get_cpds(node).state_names[node]
+            trained_states[node] = states
+
         for variable in model.nodes():
             cpd = model.get_cpds(variable)
             states = cpd.state_names[variable]
             print(f"Variable: {variable}, States: {states}")
-        # test_data_for_prediction = self.test_df.copy()
-        # # test_data_for_prediction.loc[:, 'Churn'] = None  # Assuming 'Churn' is the target variable
-        # # test_data_for_prediction['Churn'] = np.nan
-        # # test_data_for_prediction['Churn'] = np.nan  # or any placeholder value
-        # # self.test_df.drop('Churn', axis=1, inplace=True)
-        # # x_test = self.test_df.drop('Churn', axis=1, inplace=True)
-        #
-        # # self.test_df['Churn'] = np.nan
-        # # Predict missing variables
-        # # preds = model.predict(self.test_df)
-        #
+
+        inference = VariableElimination(model)
+        predictions = []
+
+        for index, row in self.test_df.iterrows():
+            evidence = {variable: row[variable] for variable in model.nodes() if variable != 'Churn'}
+            prediction = inference.map_query(variables=['Churn'], evidence=evidence)
+            predictions.append(prediction['Churn'])
+
+        true_labels = self.test_df['Churn']
+
+        # Confusion Matrix
+        conf_matrix = confusion_matrix(true_labels, predictions)
+        print('Confusion Matrix:\n', conf_matrix)
+
+        # Accuracy
+        accuracy = accuracy_score(true_labels, predictions)
+        print('Accuracy:', accuracy)
+
+        # ROC Curve
+        fpr, tpr, thresholds = roc_curve(true_labels, predictions)
+        roc_auc = auc(fpr, tpr)
+        print('ROC AUC:', roc_auc)
+
+        # Precision-Recall Curve
+        precision, recall, _ = precision_recall_curve(true_labels, predictions)
+        pr_auc = auc(recall, precision)
+        print('Precision-Recall AUC:', pr_auc)
+
+        # ROC Curve
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc))
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve')
+        plt.legend(loc='lower right')
+        plt.show()
+
+        # Precision-Recall Curve
+        plt.figure()
+        plt.plot(recall, precision, color='blue', lw=2, label='Precision-Recall curve (area = {:.2f})'.format(pr_auc))
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('Precision-Recall Curve')
+        plt.legend(loc='lower left')
+        plt.show()
+
+        # filtered_test_df = self.test_df.copy()
+        # # Map states in the test data to the closest matching states in the trained structure
+        # for node, states in trained_states.items():
+        #     filtered_test_df[node] = filtered_test_df[node].apply(
+        #         lambda x: min(states, key=lambda state: abs(state - x)))
+
         # inference = VariableElimination(model)
-        # # preds = inference.query(variables=[self.target_variable], evidence=x_test)
-        # # preds = [model.predict(evidence=evidence_point) for evidence_point in x_test.to_dict(orient='records')]
-        # if self.test_df is not None:
-        #     if 'Churn' in self.test_df.columns:
-        #         inference = VariableElimination(model)
+        # #
+        # predictions = []
+        # for index, row in filtered_test_df.iterrows():
+        #     # Specify evidence based on the values in the current row
+        #     evidence = {'tenure': row['tenure'], 'Contract': row['Contract'],
+        #                 'PaymentMethod': row['PaymentMethod'], 'MonthlyCharges': row['MonthlyCharges'],
+        #                 'TotalCharges': row['TotalCharges']}
         #
-        #         # Drop 'Churn' column from x_test
-        #         x_test_without_churn = self.test_df.drop('Churn', axis=1)
+        #     # Use the model for prediction
+        #     prediction = inference.map_query(variables=[self.target_variable], evidence=evidence)
+        #     predictions.append(prediction[self.target_variable])
         #
-        #         # Convert x_test_without_churn to a list of dictionaries
-        #         evidence_list = x_test_without_churn.to_dict(orient='records')
+        # # Evaluate performance
+        # true_labels = self.test_df[self.target_variable]
+        # accuracy = accuracy_score(true_labels, predictions)
+        # confusion_mat = confusion_matrix(true_labels, predictions)
+        # classification_rep = classification_report(true_labels, predictions)
         #
-        #         # Make predictions for the 'Churn' variable for each row
-        #         y_pred_churn_list = [inference.query(variables=['Churn'], evidence=evidence)['Churn'] for evidence in
-        #                              evidence_list]
+        # print(f'Accuracy: {accuracy}')
+        # print('Confusion Matrix:\n', confusion_mat)
+        # print('Classification Report:\n', classification_rep)
         #
-        #         # Concatenate the predictions into a single DataFrame
-        #         y_pred_churn_df = pd.concat(y_pred_churn_list, axis=1).transpose()
-        #         return y_pred_churn_df
-        #     else:
-        #         print("Error: 'Churn' column not found in test dataset.")
-        #         return None
-        # else:
-        #     print("Error: Test dataset is None.")
-        #     return None
+        # # ----------------------
+        # predictions_proba = []
+        # # Calculate predicted probabilities for positive class
+        # for index, row in filtered_test_df.iterrows():
+        #     evidence = {'tenure': row['tenure'], 'Contract': row['Contract'],
+        #                 'PaymentMethod': row['PaymentMethod'], 'MonthlyCharges': row['MonthlyCharges'],
+        #                 'TotalCharges': row['TotalCharges']}
+        #     query_result = inference.map_query(variables=[self.target_variable], evidence=evidence, show_progress=False)
+        #     prediction_proba = query_result.values[1]
+        #     predictions_proba.append(prediction_proba)
+        #
+        # # Calculate ROC curve
+        # fpr, tpr, _ = roc_curve(true_labels, predictions_proba)
+        # roc_auc = auc(fpr, tpr)
+        #
+        # # Calculate precision-recall curve
+        # precision, recall, _ = precision_recall_curve(true_labels, predictions_proba)
+        #
+        # # Plot ROC curve
+        # plt.figure(figsize=(8, 8))
+        # plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+        # plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        # plt.xlabel('False Positive Rate')
+        # plt.ylabel('True Positive Rate')
+        # plt.title('Receiver Operating Characteristic (ROC) Curve')
+        # plt.legend(loc="lower right")
+        # plt.show()
+        #
+        # # Plot precision-recall curve
+        # plt.figure(figsize=(8, 8))
+        # plt.plot(recall, precision, color='blue', lw=2, label='Precision-Recall curve')
+        # plt.xlabel('Recall')
+        # plt.ylabel('Precision')
+        # plt.title('Precision-Recall Curve')
+        # plt.legend(loc="lower left")
+        # plt.show()
 
 
 class BayesianEvaluator:
@@ -333,7 +450,7 @@ if __name__ == '__main__':
     train = BayesianTrainer(args.csv_path_train, args.csv_path_test, args.net_structure_path,
                             args.net_structure_and_params_path, args.batch_size)
 
-    train.structure_train()
+    # train.structure_train()
     train.check_for_cycles()
     train.plot_structure()
 
@@ -342,8 +459,9 @@ if __name__ == '__main__':
     # train.make_predictions(mod)
     # --
 
-    model = train.train_parameters()
-    # model = train.load_parameters()
+    # model = train.train_parameters()
+
+    model = train.load_parameters()
     # predictions = train.make_predictions(model)
     train.make_predictions(model)
 
